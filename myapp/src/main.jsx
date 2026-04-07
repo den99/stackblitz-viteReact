64740df9f676a37c23dc9f251a190d747b32175b
den99 <udlr00@gmail.com>
2026-04-07 13:57:02 +0900

    特に無し 追加

--- myapp/src/main.jsx
+++ myapp/src/main.jsx
@@ -1,19 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+//import './index.css'
 //import App from './App.jsx'
 
 import books from './ch03/books'
-//import MyHello from './ch03/01_MyHello'
+import MyHello from './ch03/01_MyHello'
 //import EventBasic from './ch03/02_EventBasic'
 //import StateBasic from './ch03/03_StateBasic'
 //import ForList from './ch03/04_ForList'
 //import ForNest from './ch03/05_01_ForNest'
 import ForFilter from './ch03/06_ForFilter';
+import SelectStyle from './ch03/08_SelectStyle'
+import StyledPanel from './ch03/09_StyledPanel'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+
+    ----ForFilter----
     <ForFilter src={books} />
-    
+
+    ----SelectStyle----
+    <SelectStyle mode="dark" />
+
+    ----StyledPanel----
+    <StyledPanel>
+     <p>メンバー募集中！</p>
+     <p>ようこそ、WINGSプロジェクトへ！！</p>
+     <MyHello myName="鈴木" /> 
+    </StyledPanel>    
+
+
   </StrictMode>,
 )
