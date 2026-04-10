400748db8a51182f233a75516f4ee55baf730489
Yu <Bao.Lun@hoge.com>
2026-04-10 10:53:41 +0900

    try commit

--- myapp/src/main.jsx
+++ myapp/src/main.jsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client'
 //import App from './App.jsx'
 
 import books from './ch03/books'
-import MyHello from './ch03/01_MyHello'
+//import MyHello from './ch03/01_MyHello'
 //import EventBasic from './ch03/02_EventBasic'
 //import StateBasic from './ch03/03_StateBasic'
 //import ForList from './ch03/04_ForList'
