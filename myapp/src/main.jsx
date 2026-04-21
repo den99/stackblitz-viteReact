af3b6c0efbdfc1ff6693d68f3727e9335eac1b7b
den99 <udlr00@gmail.com>
2026-04-21 11:50:25 +0900

    ListTemplate追加

--- myapp/src/main.jsx
+++ myapp/src/main.jsx
@@ -3,9 +3,9 @@ import { createRoot } from 'react-dom/client'
 //import './index.css'
 //import App from './App.jsx'
 
-import './ch02/class.css'
-import Tic from './ch02/tic-tac-toe'
-/*
+//import './ch02/class.css'
+//import Tic from './ch02/tic-tac-toe'
+
 import books from './ch03/books'
 //import MyHello from './ch03/01_MyHello'
 //import EventBasic from './ch03/02_EventBasic'
@@ -15,15 +15,12 @@ import books from './ch03/books'
 import ForFilter from './ch03/06_ForFilter';
 import SelectStyle from './ch03/08_SelectStyle'
 import StyledPanel from './ch03/09_StyledPanel'
-*/
+import ListTemplate from './ch03/12_ListTemplate'
+
 createRoot(document.getElementById('root')).render(
 
-//  <StrictMode>
-<>
-
-  <Tic />
-</>
-/*
+  <StrictMode>
+{/*
     ----ForFilter----
     <ForFilter src={books} />
 
@@ -36,8 +33,20 @@ createRoot(document.getElementById('root')).render(
      <p>ようこそ、WINGSプロジェクトへ！！</p>
      StyledPanel.js
     </StyledPanel>    
+*/}
+    <ListTemplate src={books}>
+      {elem => (
+        <>
+       <dt>
+         <a href={`https://wings.msn.to/books/${elem.isbn}/${elem.isbn}.jpg`}>
+             {elem.title}（{elem.price}円）
+         </a>
+       </dt>
+       <dd>{elem.summary}</dd>
 
+        </>
+      )}
+    </ListTemplate>
 
   </StrictMode>,
-*/
   )
